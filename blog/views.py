ff6cae3a9d034ff9076a46fb4302346351bb83ec
José Loarca <joseloarca97@icloud.com>
2022-02-27 18:44:44 -0600

    migrate function views to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Post
 
 
 # Create your views here.
 
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-
-    return render(request, 'blog/index.html', {'posts': latest_posts})
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/all-posts.html', {'posts': all_posts})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def post_details(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-details.html', {'post': post, 'tags': post.tags.all()})
+class AllPostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
+
+
+class PostDetailView(DetailView):
+    template_name = 'blog/post-details.html'
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = self.object.tags.all()
+        return context
